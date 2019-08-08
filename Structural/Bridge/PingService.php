20314168411b8b58278117f3a58cefb814149603
Moneystream2012 <andrey.installmonster@gmail.com>
2019-08-08 10:44:57 +0300

    PingService restored

---
+++ Structural/Bridge/PingService.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace DesignPatterns\Structural\Bridge;
+
+class PingService extends Service
+{
+    public function get(): string
+    {
+        return $this->implementation->format('pong');
+    }
+}
